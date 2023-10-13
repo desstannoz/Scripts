b77d1aa3325607780219d2f13c233963bb436395
Kazim Ozkurt <kazim_ozkurt2@hotmail.com>
2023-10-13 13:37:24 +0300

    script

--- ytnk.js
+++ ytnk.js
@@ -1,6 +1,6 @@
 // ==UserScript==
-// @name
-// @namespace
+// @name       ytnk automation
+// @namespace ytnk
 // @version      0.1
 // @description  Automation for ytnk
 // @author       Kazim Ozkurt
